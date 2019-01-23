9381637bfb9187a7a4700ddb3dd405600b2cad69
Max Liao <pansong.liao@togic.com>
2019-01-23 15:38:45 +0800

    commit

--- run_classifier_word.py
+++ run_classifier_word.py
@@ -229,6 +229,11 @@ class NewsProcessor(DataProcessor):
         return self._create_examples(
             self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
 
+    def get_test_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "test.tsv")), "test")
+
     def get_labels(self):
         """See base class."""
         #return list(self.labels)
