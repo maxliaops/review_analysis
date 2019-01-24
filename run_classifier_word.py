29b91b8e923976c914b4f601d4809aec9a10d525
Max Liao <pansong.liao@togic.com>
2019-01-24 12:17:58 +0800

    commit

--- run_classifier_word.py
+++ run_classifier_word.py
@@ -28,8 +28,8 @@ import tokenization_word as tokenization
 import tensorflow as tf
 
 #logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(message)s', 
-                    datefmt = '%m/%d/%Y %H:%M:%S',
-                    level = logging.INFO)
+#                    datefmt = '%m/%d/%Y %H:%M:%S',
+#                    level = logging.INFO)
 #logger = logging.getLogger(__name__)
 
 
